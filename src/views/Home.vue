19177ce9e480d774a3e802cc0049595e83640c99
Brandsma <a.brandsma@protonmail.com>
2020-11-15 12:11:40 +0100

    github pages deployment

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <RiskInterface />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import RiskInterface from "@/components/RiskInterface.vue";
+
+export default {
+  name: "Home",
+  components: {
+    RiskInterface
+  }
+};
+</script>
